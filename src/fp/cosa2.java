ee3d59a2458fcacd67dcec0a06f8dcb2782358c5
josemarialuna <josemarialunaromera@gmail.com>
2023-02-15 16:40:00 +0100

    test

--- src/fp/cosa2.java
+++ src/fp/cosa2.java
@@ -2,5 +2,6 @@ package fp;
 
 public class cosa2 {
     private String thisisavariable;
+    
     private int thisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfathisisahafgsfa;
 }
